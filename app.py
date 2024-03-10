13c2a41bfb1f738a0af5d91edff02264015b49e2
Mihir Pesswani <pesswani.mihir@gmail.com>
2024-03-10 23:33:47 +0530

    endpoint to send email

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
+from config import MailBody
+from mailer import send_mail
 import uvicorn
 
 app = FastAPI()
@@ -9,5 +11,12 @@ def index():
     return{"status": "Mail Server is Running!!!"}
 
 
+@app.post("/send_mail")
+async def schedule_mail(req:MailBody, tasks:BackgroundTasks):
+    data = req.dict()
+    tasks.add_task(send_mail, data)
+    return {"status": 200, "message": "Mail Scheduled Successfully"}
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host='127.0.0.1', reload=True)
+    uvicorn.run('app:app', host='127.0.0.1', reload=True)
