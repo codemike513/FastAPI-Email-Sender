85fc503f9c8d01d4a24618f857f18f1c0392b3bd
Mihir Pesswani <pesswani.mihir@gmail.com>
2024-03-10 23:05:46 +0530

    mail model

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel
+from typing import List
 
 load_dotenv()
 
@@ -7,3 +9,8 @@ HOST = os.environ.get("MAIL_HOST")
 USERNAME = os.environ.get("MAIL_USERNAME")
 PASSWORD = os.environ.get("MAIL_PASSWORD")
 PORT = os.environ.get("MAIL_PORT", 465)
+
+class MailBody(BaseModel):
+    to: List[str]
+    subject: str
+    body: str
